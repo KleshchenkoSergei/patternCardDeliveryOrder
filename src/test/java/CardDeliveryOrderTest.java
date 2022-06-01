f9da257f3c06906d42f6f037b4ffab333e5b6408
Sergei Kleshchenko <kserv2006@yandex.ru>
2022-06-01 23:20:24 +0700

    Check CI

--- src/test/java/CardDeliveryOrderTest.java
+++ src/test/java/CardDeliveryOrderTest.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.commands.IsDisplayed;
 import com.github.javafaker.Faker;
 import lombok.Data;
@@ -23,6 +24,7 @@ public class CardDeliveryOrderTest {
 
     @Test
     public void shouldFakerTest() throws IOException {
+//        Configuration.headless = true;
         open("http://localhost:9999"); // open webpage
 
         //get data
@@ -38,7 +40,8 @@ public class CardDeliveryOrderTest {
         $("[data-test-id=\"phone\"] [name=\"phone\"]").setValue(generator.generate("ru").getPhone()); // input phone
         $("[class=\"checkbox__text\"]").click(); //check agreement
         $$("button[class*=\"button\"] ").findBy(text("Запланировать")).click(); // press order button
-        $("[data-test-id=\"success-notification\"] [class=\"notification__content\"]").shouldHave(text("Встреча успешно запланирована на " + actualDate), Duration.ofMillis(15000)); //check registration
+//        $("[data-test-id=\"success-notification\"] [class=\"notification__content\"]").shouldHave(text("Встреча успешно запланирована на " + actualDate), Duration.ofMillis(15000)); //check registration
+        $("[data-test-id=\"success-notification\"] [class=\"notification__content\"]").shouldHave(visible, (text("Встреча успешно запланирована на " + actualDate))); //check registration
 
         $("[data-test-id=\"date\"] [placeholder=\"Дата встречи\"]").doubleClick().sendKeys(actualDatePlus); // input another actualDate
         $$("button[class*=\"button\"]").findBy(text("Запланировать")).click(); // press order button
